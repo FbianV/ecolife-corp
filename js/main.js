fee6c19a475f6a16abff6ee4801b1d81e85c09c6
Fabián Vergara <66499768+FbianV@users.noreply.github.com>
2022-05-08 21:57:47 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+define(["require", "exports", "jquery"], function (require, exports, jquery) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var $ = jquery;
+    (function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.querySelectorAll('.needs-validation');
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    })();
+});
