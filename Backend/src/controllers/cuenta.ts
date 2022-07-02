41a2929d396e40ab5e50dcef7ce2b1c0292332b5
Mauricio Castillo <mauricio.castillo.b@mail.pucv.cl>
2022-07-02 18:49:44 -0400

    get users

--- Backend/src/controllers/cuenta.ts
+++ Backend/src/controllers/cuenta.ts
@@ -1,7 +1,16 @@
 import {Request, Response} from 'express';
+import {database} from '../index';
+import {ref, onValue} from "firebase/database";
 
 export const getCuenta=(req:Request, res:Response)=>{
-
+    const proyectos = ref(database, 'usuarios/');
+    var data;
+    onValue(proyectos, (snapshot) => {
+    data = snapshot.val();
+    console.log(data);
+    res.status(200).send(JSON.stringify(data));
+    });
+   
 
 }
 
